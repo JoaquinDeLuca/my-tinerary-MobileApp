491fed75f5209697e3850a14129afbdfc602c089
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-27 16:08:19 -0300

    se agrega home y welcome

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import { View } from 'react-native'
+import React from 'react'
+import SignInWelcome from '../components/SignInWelcome'
+
+export default function WelcomeScreen() {
+  return (
+    <View>
+        <SignInWelcome />
+    </View>
+  )
+}
