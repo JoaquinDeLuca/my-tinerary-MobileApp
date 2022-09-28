f22096ff617074a23d82ebc2e1d3be528e9ea196
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-28 20:13:24 -0300

    funcionando inicio de session

--- src/components/SignInWelcome.js
+++ src/components/SignInWelcome.js
@@ -1,31 +1,90 @@
-import { View, Text, TextInput, Button, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Button, Image, StyleSheet, Alert} from 'react-native'
+import { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useSignInUserMutation } from '../features/userApi'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function WelcomeScreen() {
 
+    const [signInUser] = useSignInUserMutation()
 
+    const [login, setLogin] = useState({
+        email: "",
+        password: "",
+        from: "form",
+    })
+
+    const captureData = (text, name) => {
+        setLogin({...login, [name]: text })
+    }
+    // console.log(login)
+
+    const saveData = () => {
+        const userDate = {
+            mail: login.Email,
+            password: login.Password,
+            form: 'form'
+        }
+        console.log(userDate)
+
+        signInUser(userDate)
+            .then( response => {
+                try{
+                    if(response.data.success === true){
+                        AsyncStorage.setItem('token',JSON.stringify(response.data.response.token))
+                        // aca pasaria el verifyToken
+                        console.log('______OK______')
+                    }else{
+                        // alert of error
+                        console.log('_____INCORRECT_____')
+                        console.log(response)
+                        Alert.alert(
+                            'Incorrect',
+                            'password or email, try again',
+                        )
+                    }
+                } catch(error){
+                    console.log('___USER NONEXISTENT___')
+                    Alert.alert(
+                        'User',
+                        'nonexistent',
+                    )
+                }
+            })
+    }
+
+    // AsyncStorage.getItem('token').then(response => console.log(response))
   return (
     <SafeAreaView style={styles.container}>
-
         <Image 
             style={styles.logo} 
             source={{uri:'https://i.postimg.cc/Fs8QT5YB/logo3.png'}} type="logo" 
         />
         <Text style={styles.titleText}>Welcome!</Text>
         <Text style={styles.p}>Log in to continue</Text>
+        <View>
+            <TextInput 
+                style={styles.input}
+                placeholder='Email'
+                name='Email'
+                onChangeText={text => captureData(text, 'Email')}
+            ></TextInput>
 
+            <TextInput 
+                style={styles.input}
+                placeholder='Password'
+                name='Password'
+                secureTextEntry={true}
+                onChangeText={ text => captureData(text, 'Password')}
+            ></TextInput>
 
-        <Text>Email</Text>
-        <TextInput style={styles.input}></TextInput>
-
-        <Text>Password</Text>
-        <TextInput style={styles.input}></TextInput>
-
-        <Button title='Sign In'></Button>
+        </View>
+        <View style={styles.btnContainer}>
+            <Button color='#256D85' onPress={() => saveData()} title='Sign In'></Button>
+        </View>
 
         <Text style={styles.message}>don't you have an account?</Text>
-        <Button title='Sign Up Now'></Button>
+        <Button color='#256D85' title='Sign Up Now'></Button>
 
     </SafeAreaView>
   )
@@ -54,10 +113,15 @@ const styles = StyleSheet.create({
         borderRadius: 7,
         padding: 8,
         width: 250,
-        height: 35
+        height: 35,
+        margin: 10
     },
     message:{
-        color:'#627D98',
+        color:'#7895B2',
         margin: 10,
     },
+    btnContainer:{
+        width: 100,
+        margin: 10
+    }
 })
