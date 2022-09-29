18017ef3b4b81c94d2bd8e8affc66731e0c2a25f
LilAguh <aguezeochoa@gmail.com>
2022-09-28 23:10:16 -0300

    se agrega la vista a cities

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 // import { NavigationContainer } from '@react-navigation/native';
-
+import { Provider } from 'react-redux'
+import store from './src/features/store'
 import HomeScreen from './src/screens/HomeScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen'
+import CitiesScreen from './src/screens/CitiesScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <HomeScreen  /> */}
-      <WelcomeScreen />
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        {/* <HomeScreen  /> */}
+        {/* <WelcomeScreen /> */}
+        <CitiesScreen />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0000',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
