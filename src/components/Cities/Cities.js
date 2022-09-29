4ced62ead0f81104e7f68fe3dba83206b900171c
LilAguh <aguezeochoa@gmail.com>
2022-09-29 13:46:57 -0300

    se agregla la busqueda de ciudades

--- src/components/Cities/Cities.js
+++ src/components/Cities/Cities.js
@@ -1,4 +1,4 @@
-import { View, TextInput, StyleSheet, ScrollView } from 'react-native'
+import { View, TextInput, Text, StyleSheet, ScrollView } from 'react-native'
 import { useState, useRef } from "react"
 import { useGetAllCitiesQuery } from '../../features/citiesApi'
 import React from 'react'
@@ -9,16 +9,26 @@ import City from './City'
 export default function CitiesScreen() {
 
     const searchInput = useRef()
-    const [value, setValue] = useState("")
-    const searchValue = () => {
-        setValue(searchInput.current.value)
+    const [value, setValue] = React.useState("");
+    const searchValue = (value) => {
+        setValue(value)
     }
-
     const { data: cities } = useGetAllCitiesQuery(value)
+
+    // const nullCity = (cities) => {
+    //     if (cities > 0) {
+    //         return (cities?.map(cities => <City key={cities.city} name={cities.city} data={cities} />))
+    //     }
+    //     else {
+    //         return (
+    //             <Text>Not found cities</Text>
+    //         )
+    //     }
+    // }
 
     return (
         <View style={styles.CitiesContainer}>
-            <TextInput style={styles.input} name="text" type="text" placeholder="Find city:" ref={searchInput} onChange={searchValue}></TextInput>
+            <TextInput style={styles.input} name="city" type="text" placeholder="Find city:" ref={searchInput} onChangeText={searchValue} value={value} />
             <ScrollView style={styles.CitiesScroll}>
                 {cities?.map(cities => <City key={cities.city} name={cities.city} data={cities} />)}
             </ScrollView>
@@ -37,12 +47,13 @@ const styles = StyleSheet.create({
     },
     input: {
         borderWidth: 1,
-        borderColor: '#000',
+        borderColor: "#B8466860",
         color: "#000",
         borderRadius: 15,
-        marginBottom: 40,
-        marginTop: 80,
+        marginBottom: 20,
+        marginTop: 20,
         width: 250,
-        height: 40
+        height: 40,
+        paddingHorizontal: 20,
     }
 })
