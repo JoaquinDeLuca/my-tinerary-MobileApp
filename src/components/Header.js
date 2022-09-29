684aa6fc279d27b153c417a023e5edd4a1224d96
LilAguh <aguezeochoa@gmail.com>
2022-09-29 13:44:12 -0300

    se agrega un header

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React from 'react'
+import acceso from "../../assets/logo/acceso.png"
+import logo1 from "../../assets/logo/logo1.png"
+
+export default function Header() {
+
+
+    return (
+        <View style={styles.HeaderContainer} >
+            <Image source={logo1} style={styles.HeaderLogo1} />
+            <Image source={acceso} style={styles.HeaderAcceso} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    HeaderContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+        // backgroundColor: "#B84668",
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderBottomColor: "#B8466860",
+        borderStyle: 'solid',
+        borderBottomWidth: 1
+    },
+    HeaderImageContainer: {
+    },
+    HeaderAcceso: {
+        resizeMode: 'stretch',
+        width: 40,
+        height: 40,
+        margin: 10,
+        tintColor: "#B84668"
+    },
+    HeaderLogo1: {
+        resizeMode: 'stretch',
+        width: 80,
+        height: 40,
+        margin: 10,
+        tintColor: "#B84668"
+    }
+})
