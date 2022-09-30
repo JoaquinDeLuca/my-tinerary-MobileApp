d350c1ac83eb68b0c90fbad8f65c22be134488bd
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-30 13:00:34 -0300

    se agrego data user

--- src/navigation/BottomTab.js
+++ src/navigation/BottomTab.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Animated, Dimensions, Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
@@ -11,10 +10,37 @@ import Home from '../../assets/icons/home.png'
 import Tinerary from '../../assets/icons/logo1.png'
 import Aguh from '../../assets/icons/Aguh.jpg'
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import { setCredentials, deleteCredentials } from '../features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 
 const Tab = createBottomTabNavigator();
 export default function BottomTab({ navigation }) {
+
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.userr)
+
+    // console.log(user)
+    useEffect(() => {
+        if (AsyncStorage.getItem('user')) {
+        getData();
+        // console.log('Hi!')
+        }
+    }, [])
+  
+    const getData = async () => {
+        try {
+        const jsonValue = await AsyncStorage.getItem('user')
+        return jsonValue != null ? dispatch(setCredentials((JSON.parse(jsonValue)))): null;
+        } catch(e) {
+        // error reading value
+        }
+    }
+
+
     return (
         // <NavigationContainer>
         <Tab.Navigator tabBarOptions={{
@@ -51,8 +77,13 @@ export default function BottomTab({ navigation }) {
                 options={{
                     headerShadowVisible: false,
                     tabBarIcon: ({ focused }) => (
+                         user === null ?
                         <View>
-                            <Image source={Aguh} style={focused ? styles.NavBarUserActive : styles.NavBarUser} />
+                            <Image source={{uri: user.photo }} style={focused ? styles.NavBarUserActive : styles.NavBarUser} />
+                        </View>
+                        : 
+                        <View>
+                            <Image source={{uri:'https://i.postimg.cc/J42nVhXL/usuario.png'}} style={focused ? styles.NavBarUserActive : styles.NavBarUser} />
                         </View>
                     )
                 }} />
@@ -77,7 +108,7 @@ const styles = StyleSheet.create({
         height: 32,
         borderRadius: 100,
         borderWidth: 3,
-        borderColor: "#B84668"
+        // borderColor: "#B84668"
     },
     NavBarUser: {
         width: 32,
