fd7e4404a9d6d78f3ddcba1e0fdc9c69ca132d58
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-30 13:01:01 -0300

    se agrego el slice user

--- src/features/store.js
+++ src/features/store.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import userApi from './userApi'
 import citiesApi from './citiesApi'
 import itinerariesApi from './itinerariesApi'
+import userSlice from './userSlice'
 
 
 export default configureStore({
     reducer: {
         [userApi.reducerPath]: userApi.reducer,
         [citiesApi.reducerPath]: citiesApi.reducer,
-        [itinerariesApi.reducerPath]: itinerariesApi.reducer
+        [itinerariesApi.reducerPath]: itinerariesApi.reducer,
+        userr: userSlice
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
